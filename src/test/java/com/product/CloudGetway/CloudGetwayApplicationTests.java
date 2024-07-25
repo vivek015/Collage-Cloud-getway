07344f408ba5d8c3a9b7df76d091ba6d3f5e0fe5
vivek015 <vsvivek123singh@gmail.com>
2024-07-25 12:56:14 +0530

    first commit

---
+++ src/test/java/com/product/CloudGetway/CloudGetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.CloudGetway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGetwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
